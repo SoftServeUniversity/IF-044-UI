788992af6db3b1df6727d45dbf132b05adfd7fbb
Test user <rostyslavchaban@gmail.com>
2014-05-23 16:42:01 +0300

    new login module

--- project/js/components.js
+++ project/js/components.js
@@ -1,73 +1,77 @@
-  (function() { 
-    var loginButton = document.getElementById('loginButton');
-	var email = document.getElementById('inputEmail3');
-    var password = document.getElementById('inputPassword3');
-    var closeButton = document.getElementById('closeButton');
-    var res1, res2, currentUser; 
+  (function() {
+      var loginButton = document.getElementById('loginButton');
+      var email = document.getElementById('inputEmail3');
+      var password = document.getElementById('inputPassword3');
+      var closeButton = document.getElementById('closeButton');
+      var res1, res2, currentUser;
 
-	        function errorEmail() {
-               console.log('Не правильно введенна Пошта');
-            };
+      function errorEmail() {
+          console.log('Не правильно введенна Пароль або Пошта');
+      };
 
-             function errorPassword() {
-             	console.log('Не правильно введенний Пароль'); 
-             };
+      function errorPassword() {
+          console.log('Не правильно введенний Пароль або Пошта');
+      };
 
-    function postWishMsg() {
-       alert('Вітаю ' + Users[currentUser].name +'!')
-    }         
+      function postWishMsg() {
+          alert('Вітаю ' + Users[currentUser].name + '!')
+      }
 
-     function clearForms() {
-       closeButton.onclick = function () {
-         return true;
-       };
-     }        
-  
-    loginButton.onclick = function() {
- 
-        	
-        	for(var i = 0; i<Users.length; i++ ) {
-        		if(Users[i].email == email.value) {
-        			res1 = true;
-        			currentUser = i;
-        			break;
-        		} else {
-        			res1 = false;
-        		};
-        		
-        	}; 
-       
-       
-        	
-        	if(currentUser >= 0) {
-        			if(Users[currentUser].password == password.value){
-        				res2 = true;
-        			} else {
-        				res2 = false;
-        			};
-        			
-        		};
-        		
-       
-        
+      function clearForms() {
+          closeButton.onclick = function() {
+              return true;
+          };
+      }
 
-        var Error = false;
-        
-      
-        if(res1 == false){
-             errorEmail();
-             Error = true;
-        };
+      loginButton.onclick = function() {
 
-        if(res2 == false){
-             errorPassword()
-             Error = true;
-        };
 
-        if(!Error){ 
-            postWishMsg();
-            clearForms();
-        };
-        return false;
-    };
-})()
+          for (var i = 0; i < Users.length; i++) {
+              if (email.value == Users[i].email) {
+                  res1 = true;
+                  currentUser = i;
+                  break;
+              } else if (email.value == Users[i].username) {
+                  res1 = true;
+                  currentUser = i;
+                  break;
+              } else {
+                  res1 = false;
+              };
+
+          };
+
+
+
+          if (currentUser >= 0) {
+              if (Users[currentUser].password == password.value) {
+                  res2 = true;
+              } else {
+                  res2 = false;
+              };
+
+          };
+
+
+
+
+          var Error = false;
+
+
+          if (res1 == false) {
+              errorEmail();
+              Error = true;
+          };
+
+          if (res2 == false) {
+              errorPassword()
+              Error = true;
+          };
+
+          if (!Error) {
+              postWishMsg();
+              clearForms();
+          };
+          return false;
+      };
+  })()
