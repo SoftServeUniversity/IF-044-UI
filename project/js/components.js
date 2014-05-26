b5f724804c49a3ea22c7475df17032cc384ea8cd
Kapechuk <y.kapechuk@gmail.com>
2014-05-26 13:53:04 +0300

    new Tests Filter Module

--- project/js/components.js
+++ project/js/components.js
@@ -72,3 +72,63 @@ function loginModule() {
           return false;
       };
   }
+
+
+
+  function testsFilter() {
+
+     var textBox = document.getElementById('testBox');
+     var select = document.getElementById('selectForm');
+
+     function createTestElement(i) {
+       var newDiv = document.createElement('div');
+       newDiv.className = 'test';
+       var p = document.createElement('p');
+       p.innerHTML = 'Тест: ';
+       var a = document.createElement('a');
+       a.innerHTML = Tests[i].name;
+       p.appendChild(a);
+       newDiv.appendChild(p);
+       textBox.appendChild(newDiv);
+     };
+
+          Tests.sort(function (a, b) {return a.date - b.date});
+       
+          for(var i = 0; i<Tests.length; i++) {
+            createTestElement(i);
+          };     
+
+     selectForm.onchange = function () {
+      textBox.innerHTML = '';
+
+        if(this.value === '1') {
+
+         Tests.sort(function (a, b) {return a.date - b.date});
+       
+          for(var i = 0; i<Tests.length; i++) {
+            createTestElement(i);
+          };
+
+        };
+
+        if(this.value === '2') {
+          
+          Tests.sort(function (a, b) {return a.name > b.name});
+
+            for(var i = 0; i<Tests.length; i++) {
+            createTestElement(i);
+          };
+        };
+
+        if(this.value === '3') {
+
+          Tests.sort(function (a, b) {return a.category - b.category});
+            
+            for(var i = 0; i<Tests.length; i++) {
+            createTestElement(i);
+          };
+
+        };
+
+     }; 
+  }
