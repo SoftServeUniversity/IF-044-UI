bc9a048e08078b298e887779e4e84efdc33f8d33
Sergiy Ryazanov <454105503qwerty@gmail.com>
2014-05-28 13:45:16 +0300

    28.05

--- project/js/statistic.js
+++ project/js/statistic.js
@@ -5,12 +5,20 @@ function statisticmodule () {
 	var People = document.getElementById('result');
 	var Score = document.getElementById('result1');		
 	
-  var avarege, scoreSum,globalScore,globalUser;
+  var avarege, 
+      scoreSum,
+      globalScore,
+      globalUser,
+      todayDay,
+      todayMonth,
+      lastDay = [],
+      Score = [],
+      Users = []
 
-var summa = function(m) {
-for(var s = 0, k = m.length; k; s += m[--k]);//обрахунок сумми елементів массиву
-return s;
-};
+    var summa = function(m) {
+    for(var s = 0, k = m.length; k; s += m[--k]);//обрахунок сумми елементів массиву
+    return s;
+    };
 	function peopleadd(b){
 		People.innerHTML = 'Користувачів:'+b;
     }
@@ -19,30 +27,65 @@ return s;
     }
    
 
-var all = function() {
-	globalScore=0;
-	for(var i = 0; i < Users.length;  i++){
-		if (Users[i].testScore) {
-		globalScore+=(summa(Users[i].testScore)/Users[i].testScore.length);
-		};
-	}
-	return globalScore;
+    
+    var getDay = function(){
+      var  j = 0
+      var date = new Date();
+        todayDay = date.getDate();
+           for (var i = todayDay; i > (todayDay-7); i--) {
+                lastDay[j] = i;
+                j++
+            }
+            return lastDay
+    } 
+
+var getMonth = function () {
+    var data= new Date();
+    todayMonth = data.getMonth();
+    return(todayMonth);
+}   
+
+var score = function(day,month) {
+var dayScore=[], j = 0;
+ for (var i = 0; i < Result.length; i++) {
+     if (day == Result[i].day && month == Result[i].month) {
+        dayScore[j] = Result[i].score;
+        j++ 
+     };
+ };
+var avarage = summa(dayScore)/dayScore.length;
+Users.push(dayScore.length);
+Score.push(avarage);
+return(Score);
 }
-var userPast = function(){
-globalUser=0;
-for(var i = 0; i < Users.length;  i++){
-	if (Users[i].testScore) {
-globalUser++;
-	}
-}
-return globalUser;
+var result = function(){
+    for (var i = 0; i < lastDay.length; i++) {
+        score(lastDay[i],todayMonth);
+    if (isNaN(Score[i])) {
+            Score[i] = 0;
+        
+};
+        
+    };
+    return
 }
 
-avarege = (all()/userPast());
-scoreadd(avarege);
-peopleadd(userPast());
 
 
+getDay();
+getMonth();
+result();    
+    
+var a = [1, 2, 3];
+alert(a.concat(4, 5, 6));
+alert(a.concat([4, 5], [6]));     
+var b = Score[1]+Score[2]+Score[3]+Score[4]+Score[5]+Score[6];
+console.log(b);
+
+for (var i = 1; i < Score.length; i++) {
+    var c =+ Score[i];
+};
+console.log(c);
 $(function () {
         $('#container').highcharts({
             title: {
@@ -54,7 +97,9 @@ $(function () {
                 x: -20
             },
             xAxis: {
-                categories: ['1день', '2день', '3день', '4день', '5день', '6день','7день']
+                categories: [lastDay[0]+'.'+(todayMonth+1), lastDay[1]+'.'+todayMonth, 
+                lastDay[2]+'.'+todayMonth, lastDay[3]+'.'+todayMonth, 
+                lastDay[4]+'.'+todayMonth, lastDay[5]+'.'+todayMonth,lastDay[6]+'.'+todayMonth]
             },
             yAxis: {
                 title: {
@@ -75,10 +120,10 @@ $(function () {
             },
             series: [{
                 name: 'Користувачі',
-                data: [userPast()]
+                data: [Users[0],Users[1],Users[2],Users[3],Users[4],Users[5],Users[6],Users[7]]
             }, {
             name: 'Середній бал',
-                data: [avarege = (all()/userPast())]
+                data: [Score[0],Score[1],Score[2],Score[3],Score[4],Score[5],Score[6],Score[7]]
             }]
         });
     });
