21421427c70d741c79af44277074be7f034fbc22
Serhiy Yosipenko <serhiy.yosipenko@gmail.com>
2014-06-03 13:34:47 +0300

    ...

--- project/js/category_search.js
+++ project/js/category_search.js
@@ -10,16 +10,16 @@ window.onload = function CategoriesController () {
 		}
 	}
 	for(var i = 0; i<cat_array.length; i++){
-	    content +='<div class="col-xs-12"><div class=" col-xs-8 col-xs-offset-2 text-left name-category">'+cat_array[i].name+'</div>';
+	    content +='<div class="col-xs-12"><div class=" col-xs-8 col-xs-offset-2 text-left">'+cat_array[i].name+'</div>';
 	    for(var j = 0;j<Categories.length;j++){
 	    if(Categories[j].parent_id == cat_array[i].id){
-	       content +='<div class="row"><div class="col-xs-8 col-xs-offset-1 text-center" sub_categ ="'+Categories[j].id+'"  categ ="'+Categories[j].parent_id+'">'+Categories[j].name+'</div></div>';        
+	       content +='<div class="row"><div class="col-xs-8 col-xs-offset-1 text-center">'+Categories[j].name+'</div></div>';        
 	    }
 	    }
 	     content += '</div>';
 	}
 	     category.innerHTML += content;
-	    //
+	    /////
 	     var table = document.getElementById("table_result");
 	     for(i=0; i < Tests.length;i++) {
 	       var row = table.insertRow(table.rows.length);
