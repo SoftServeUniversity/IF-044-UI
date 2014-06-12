13fa52a4dd1e3965e58dccf76024393f615ef49b
dreadnough <odin_ogame@ukr.net>
2014-06-12 15:25:06 +0300

    fixed profile

--- project/js/prof_change.js
+++ project/js/prof_change.js
@@ -1,17 +1,26 @@
 window.onload = function prof_change() {
 	console.log(Model.date);
-	var last_register_user = Model.date.Users[Model.date.Users.length-1];
-	console.log(last_register_user);
+	var current_user = getCurrentUser(); 
+
+    function getCurrentUser() {
+    	for (var i = 0; i< Model.date.Users.length; i++){
+    		if(Model.date.Users[i].id === Model.date.session_user_id){
+    			return Model.date.Users[i];
+    		}
+    	}
+    }
+
+	console.log(current_user);
 	
-	var first_name = last_register_user.first_name;
-	var last_name = last_register_user.last_name;
-	var passwd = last_register_user.password;
-	var re_passwd = last_register_user.re_passwd;
-	var email = last_register_user.email;
-	var birthday = last_register_user.birthday;
-	var org_troop1 = last_register_user.org_troop;
-	var org_region1 = last_register_user.org_region;
-	var org_level1 = last_register_user.org_level;
+	var first_name = current_user.firstName;
+	var last_name = current_user.lastName;
+	var passwd = current_user.password;
+	var re_passwd = current_user.re_passwd;
+	var email = current_user.email;
+	var birthday = current_user.birthday;
+	var org_troop1 = current_user.org_troop;
+	var org_region1 = current_user.org_region;
+	var org_level1 = current_user.org_level;
 
 	document.getElementById("first_name2").innerHTML = first_name;
 	document.getElementById("last_name2").innerHTML = last_name;
