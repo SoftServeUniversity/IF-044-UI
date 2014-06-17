acb622d9901b5619a407194a0c073bc859f9930c
Ivan Chyr <ichyr@softserveinc.com>
2014-06-17 19:33:05 +0300

    all is in place. infinite loop.

--- project/js/utils/category.js
+++ project/js/utils/category.js
@@ -2,7 +2,7 @@ function Category() {};
 
 Category.subcategory_path = "subcategory.html";
 Category.test_path = "test.html";
-Category.content_id = "content";
+Category.content_id = "category-page-content";
 Category.list = Model.date.Tests_categories;
 Category.test_list = Model.date.Tests;
 
@@ -13,17 +13,18 @@ Category.test_list = Model.date.Tests;
 //  active_categories_list - array of actual categories
 // 
 Category.getSubcategories = function(category_id, active_categories_list) {
-  var result = active_categories_list;
-  for (var i = result.length - 1; i >= 0; i--) {
-    if (result[i].parent_id != category_id) {
-      result.splice(i,1)
+  alert("getSubcategories");
+  var result = [];
+  for (var i = active_categories_list.length - 1; i >= 0; i--) {
+    if (active_categories_list[i].parent_id === category_id) {
+      result.push(active_categories_list[i]);
     }; 
   };
   return result;
 }
 
 // 
-// Returns list of first child subcategories
+// Returns list of root categories
 // 
 // active_categories_list - array of actual categories
 // 
@@ -31,18 +32,77 @@ Category.getRootCategories = function(active_categories_list) {
   return Category.getSubcategories(0, active_categories_list);
 }
 
+//
+//  Returns the list of tests that have the given subcategory as a parent
+//
+//  category   -  Object
+ // active_test_array - array of active tests in the system
+//
 Category.getSubCategoryTestsList = function(category, active_test_array) {
-  var result = active_categories_list;
-  for (var i = result.length - 1; i >= 0; i--) {
-    if (result[i].subcategory != category) {
-      result.splice(i,1)
+  alert("getSubCategoryTestsList");
+  var result = [];
+  console.log("getSubCategoryTestsList ->");
+  console.log("   category -> ", category);
+
+  for (var i = active_test_array.length - 1; i >= 0; i--) {
+    if (active_test_array[i].subcategory === category.id) {
+      result.push(active_test_array[i]);
     }; 
   };
+
+  console.log("   result -> ", result);
   return result;
 }
 
+Category.generate_category_page = function(category_id, active_category_list){
+  alert("generate_category_page");
+  console.log("generate_category_page ->");
+  console.log("   category_id -> " + category_id);
+  console.log("   active_category_list - >" + active_category_list.length);
+  var subcategories = Category.getSubcategories(category_id, active_category_list);
+  var content = document.getElementById(Category.content_id);
+  var last_j = 0;
+
+  console.log("Subcategories -> " + subcategories.length);
+  console.log(subcategories);
+  console.log("DOM -> " + content);
+
+  for (var i = 0; i < subcategories.length; i++) {
+    console.log("line 67");
+    console.log("Before loop iteration -> ", subcategories)
+    console.log("iteration i -> ", i);
+    var subcategory_test_list = Category.getSubCategoryTestsList(subcategories[i], Category.test_list);
+    console.log("After loop iteration -> ", subcategories)
+    console.log("test for " + subcategories[i].name + " -> " + subcategory_test_list.length)
+
+    for (var j = 0; j < subcategory_test_list.length; j--) {
+      console.log("line 75");
+      if( j % 3 == 0) {
+        content.innerHTML += '<div class="row">\n';
+      }
+      content.innerHTML += Category.generate_one_view_item(subcategories[i], j);
+      if( j % 3 == 2) {
+        content.innerHTML += '</div>\n';
+      }
+
+      last_j = j;
+    };
+
+    if ( last_j % 3 != 2) { content.innerHTML += '</div>\n'; }
+  };
+};
+
+Category.get_by_id = function(id) {
+  for ( var i = 0 ; i < Category.list.length; i++) {
+    if (Category.list[i].id == id) return Category.list[i];
+  }
+}
+
 Category.generate_one_view_item = function(parent, number) {
-  var items = getSubCategoryTestsList(parent, Category.test_list)
+  alert("generate_one_view_item");
+  console.log(" Category.generate_one_view_item -> ->")
+  var items = Category.getSubCategoryTestsList(parent, Category.test_list);
+  var items = [];
   var result = "";
 
   switch(number % 3) {
@@ -64,7 +124,8 @@ Category.generate_one_view_item = function(parent, number) {
   result += '<ul class="list-unstyled col-sm-offset-1">\n';
 
   for (var i = 0; i < items.length; i++) {
-    result += '<a href="' + Category.test_path + '?id=' + parent.id + '"> <li>' + item.name + '</li> </a>\n';
+    console.log("line 120");
+    result += '<a href="' + Category.test_path + '?id=' + parent.id + '"> <li>' + items[i].name + '</li> </a>\n';
   };
 
   result += '</ul>\n';
@@ -73,31 +134,46 @@ Category.generate_one_view_item = function(parent, number) {
   return result;
 }
 
-Category.generate_category_page = function(subcategory_id, active_test_list){
-  var subcategories = Category.getRootCategories(subcategory_id, active_test_list);
-  var content = document.getElementById(Category.content);
-
-  for (var i = 0; i < subcategories.length; i++) {
-    var sub_categories = subcategories[i].getSubCategoryTestsList(subcategories[i].id, active_test_list);
-
-    for (var i = 0; i < sub_categories.length; i--) {
-      content.innerHTML += Category.generate_one_view_item(sub_categories[i], i);
-    };
-
-  };
-};
-
-Category.getSubcategory_id = function() {
+// 
+// Obtains the number of the category from url of the page
+// 
+// input format ...?id=[value]
+// 
+// 
+Category.getCategory_id = function() {
   var temp = location.search;
   temp = temp.slice(1);
   temp = temp.split("=")[1];
   return temp * 1;
 }
 
-function generate_page(){
-  var subcategory_id = Category.getSubcategory_id();
-  Category.generate_category_page(subcategory_id, Category.list);
+// 
+// Function sets the name of the category to the page
+// and creates a link to it's statistics page
+// 
+Category.setActivePageTitle = function(active_category) {
+  var title = document.getElementById("content-title-block");
+  var stats = document.getElementById("content-title-stats");
+
+  title.innerText = active_category.name;
+  stats.href = "category-statistics.html?id=" + active_category.id;
+}
+
+// 
+// Function generates the page content
+// 
+Category.generate_page = function(){
+  var category_id = Category.getCategory_id() || 1;
+  console.log("id = " + category_id);
+  console.log("categories = " + Category.list);
+  Category.setActivePageTitle(Category.get_by_id(category_id));
+  Category.generate_category_page(category_id, Category.list);
 };
 
-window.onload = generate_page();
+function init() {
+  Model.load_localStorage();
+  Category.generate_page();
+};
+
+window.onload = init();
 
