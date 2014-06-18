f60a0c837bb9da6011d811742eb70455977306d3
Ivan Chyr <ichyr@softserveinc.com>
2014-06-18 19:44:32 +0300

    tydies HTML of admin_panel. included eltre

--- project/js/administration/about.js
+++ project/js/administration/about.js
@@ -1,7 +1,24 @@
 function AboutPageController() {};
 
+AboutPageController.data = Application.GeneralInformation.about_page;
+
 AboutPageController.init = function() {
-  console.log("asd");
+
+  var opts = {
+      lang         : 'ua',
+      styleWithCSS : false,
+      height       : 400,
+      toolbar      : 'maxi'
+  };
+  // create editor
+  $('#about-us-content').elrte(opts);
+  // or this way
+  // var editor = new elRTE(document.getElementById('our-element'), opts);
+
+  $("#about-us-content").html(AboutPageController.data);
+  
 };
 
-document.getElementById("about-us-page-click").onclick =AboutPageController.init();
+$(function() {
+    AboutPageController.init();
+});
