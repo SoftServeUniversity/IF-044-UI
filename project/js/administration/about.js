cc3048542d15d9e6f48ee9e9c6418d2eb5d5d23e
Ivan Chyr <ichyr@softserveinc.com>
2014-06-18 18:25:48 +0300

    added AboutPageController

---
+++ project/js/administration/about.js
@@ -0,0 +1,7 @@
+function AboutPageController() {};
+
+AboutPageController.init = function() {
+  console.log("asd");
+};
+
+document.getElementById("about-us-page-click").onclick =AboutPageController.init();
