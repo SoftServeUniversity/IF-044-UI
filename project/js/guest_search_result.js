cb2a3cd4f54b5f9dc9350cc9957d7c22e8ba958d
fifuck18 <tarasklyushta@gmail.com>
2014-06-24 17:01:43 +0300

    test version paginatiob on search page

--- project/js/guest_search_result.js
+++ project/js/guest_search_result.js
@@ -5,16 +5,21 @@
     row_results: document.querySelectorAll(".row-search-result")[0],
     search_input: document.getElementById('search_input'),
     linkToAdvancedSearch: document.getElementById('linkToAdvancedSearch'),
+     place_for_pagination: document.getElementById('place_for_pagination'),
     cat: false,
     subCat: false,
 	showEmpty: function (nothing) {
 		if(nothing){
-			this.row_results.innerHTML = '<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 search-result well"><p>нічого не знайдено...</p></div>';			
+			this.row_results.innerHTML = '<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 search-result well"><p>нічого не знайдено...</p></div>';	    
+             this.place_for_pagination.innerHTML = "";
 		}else{
 			this.row_results.innerHTML = '';
+            this.place_for_pagination.innerHTML = "";
 		}
     },
-    showAllcontaining: function (value) {
+    showAllcontaining: function (value,start,step) {
+                            start = (start)?start:0;
+                            step = (step)?step:4;
 							var result_array = [];
 							var array_filter_categories = document.querySelectorAll('.curent-filter');
 							if (array_filter_categories.length) {
@@ -52,9 +57,22 @@
 								}
 							}
 							if (result_array.length) {
-								for (ind in result_array) {
-									var i = result_array[ind];
+                                console.log(result_array);
+                                this.showEmpty();
+                                var lengthArray;
+                                if(start+step<result_array.length){
+                                    //lengthArray = result_array.length;
+                                    lengthArray = start+step;
+                                }else{
+                                    lengthArray = (start+1)*step;
+                                }
+                                lengthArray = (start)?start*step:step;
+                                var i;
+								for (var k = (start-1)*step; k<lengthArray;k++) {
+									//console.log("i=>"+result_array[k]);
+                                    i = result_array[k];
 									var tags = '';
+                                    if(Tests[i]){
 									for (var j = 0; j < Tests[i].tags.length; j++) {
 										if (j != Tests[i].tags.length - 1) {
 											tags += '<li><a href="">' + Tests[i].tags[j] + '&nbsp|</a></li> ';
@@ -63,11 +81,19 @@
 										}
 									}
 									this.row_results.innerHTML += '<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 search-result well"><h3 class="title-post-name"><a style="text-decoration: none;" href="Test.html?id='+Tests[i].id+'">' + Tests[i].name + '</a></h3><p class="text-justify ">' + Tests[i].description + '</p><div class="col-xs-12"><ul class="search-teg">' + tags + ' </ul></div></div>';
-								}
+									}
+									else{
+                                    //break;
+									}
+								}  
+
                                 this.searchTags();
-							} else {
+                                this.createLinksPagination(result_array.length,step,start);
+							} 
+							else {
 								this.showEmpty(true);
-        }
+							}
+        //console.log(result_array.length);
     },
 
     EventKeyUp: this.search_input.addEventListener('keyup', function () {
@@ -77,7 +103,6 @@
 																	simpleSearch.showEmpty();
 																} else {
 																	simpleSearch.showAllcontaining(value);
-                                                                    //simpleSearch.searchTags();
 																}
 															}),
      searchTags: function(){
@@ -111,12 +136,55 @@
      },
      parseUrlForSearch: function(){
         if(window.location.search){
-            var searchQuery=decodeURIComponent(window.location.search.substring(13,window.location.search.length));
-            //console.log(decodeURIComponent(escape(searchQuery)));
+            var searchQuery = this.getUrlParams().searchQuery;
              this.search_input.value = searchQuery;
-                 this.showAllcontaining(searchQuery);
+             this.showAllcontaining(searchQuery,this.getUrlParams().currentpage,4);
         }    
           
+     },
+     createLinksPagination: function(quantity, step, currentpage){
+        var countLinks  = Math.ceil(quantity/step);
+        var linkOnPage = 4;
+        currentpage = (currentpage)?currentpage: 1;
+        console.log("countLinks=>"+countLinks);
+        if(countLinks>1 && currentpage<=countLinks){ 
+            var htmlLinks = '<ul class="pagination">';
+			 var beginLink,endLink;
+			 beginLink = (currentpage-2>0)?currentpage-2:(currentpage-1>0)?currentpage-1:currentpage;
+			 endLink = (currentpage+2<=countLinks)?currentpage+2:countLinks;
+             for(var i=beginLink;i<=endLink;i++){
+                 console.log("pageLi=>"+i)
+				 if(currentpage == i){
+					htmlLinks += "<li class='active'><a href='"+window.location.pathname + "?searchQuery="+this.search_input.value+"&currentpage="+i+"'>"+i+"</a></li>";
+				 }
+				 else{
+					htmlLinks += "<li><a href='"+window.location.pathname + "?searchQuery="+this.search_input.value+"&currentpage="+i+"'>"+i+"</a></li>";
+				 }
+             }
+             htmlLinks += "</ul>";
+             this.place_for_pagination.innerHTML = htmlLinks;
+        }
+         else{
+         }
+     },
+     getUrlParams: function(){
+        var urlForParse = decodeURIComponent(window.location.search);
+        var objectParams = {},key,value;
+        if(urlForParse.length){
+             var arrayUrlForParse = urlForParse.split('&');
+             for(var i = 0;i<arrayUrlForParse.length;i++){
+                key = arrayUrlForParse[i].split('=')[0];
+                value = arrayUrlForParse[i].split('=')[1];
+                if(i == 0){
+                    objectParams[key.substring(1)] = value;
+                }
+                 else{
+                      objectParams[key] = value;
+                }
+
+             }
+        }
+         return objectParams; 
      },
 														
     init: this.EventKeyUp
@@ -126,5 +194,3 @@
 simpleSearch.init;
 simpleSearch.clickLinkToAdvancedSearch();
 simpleSearch.parseUrlForSearch();
-//simpleSearch.init2;
-//}   
