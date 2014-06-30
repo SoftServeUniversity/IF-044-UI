2846a0dd9d790ead9d1e0eee4ceb682c9e3397df
Sergiy Ryazanov <454105503qwerty@gmail.com>
2014-06-30 20:21:24 +0300

    index page

--- project/js/category_subcategory_creation.js
+++ project/js/category_subcategory_creation.js
@@ -41,24 +41,25 @@ this.listCreation = function (categories,id){
 
 	for (var i = 0; i < catName.length; i++) {
 		var newdiv = document.createElement('div');
-		newdiv.className = "col-sm-10 col-sm-offset-1 pos";	
+		newdiv.className = "col-xs-12 col-sm-3 col-lg-3 contact col-sm-offset-1";	
 		newdiv.innerHTML += '<div class="row"> <div class="col-xs-10 col-xs-offset-1 block-contact"> <div class="contact-person"><a href="category.html?id='+catId[i]+'">'+catName[i]+'</a>: '+this.categoryTestCount(catName[i])+'</div> <div class="row"> <ul class="list-unstyled col-sm-offset-1"> </ul> </div> </div> </div>';
+		document.getElementById('category').appendChild(newdiv);
 		//newdiv.innerHTML +='<strong>Категорія <a href="category.html?id='+catId[i]+'"> <u>'+catName[i]+'</u> </a> <br></strong> <span class="count">Тестів: '+this.categoryTestCount(catName[i])+'</span>';
 		// document.getElementsByClassName('col-sm-9 col-sm-offset-2')[0].appendChild(newdiv);
 		// var newul = document.createElement('ul');
 		// newul.className = ('cat list-unstyled col-sm-offset-1');
 		// document.getElementsByClassName('col-sm-4 col-sm-offset-0 col-xs-12 col-xs-offset-3')[i].appendChild(newul);
 }
-	// var catEl = document.getElementsByClassName('cat');	
-	// for (var i = 0; i< catEl.length; i++) {		
-	// 	var subcat = Application.Tests_categories[i].getSubcategories(i+1);
-	// 	for (var j = 0; j < subcat.length; j++) {
-	// 		var newdiv = document.createElement('li');
-	// 		newdiv.innerHTML = '<a href="subcategory.html?id='+subcat[j].id+'">'+subcat[j].name+'</a>';
-	// 		catEl[i].appendChild(newdiv);
-	// 	};
+	var catEl = document.getElementsByClassName('list-unstyled col-sm-offset-1');	
+	for (var i = 0; i< catEl.length; i++) {		
+		var subcat = Application.Tests_categories[i].getSubcategories(i+1);
+	for (var j = 0; j < subcat.length; j++) {
+	var newdiv = document.createElement('li');
+	newdiv.innerHTML = '<a href="subcategory.html?id='+subcat[j].id+'">'+subcat[j].name+'</a>';
+	catEl[i].appendChild(newdiv);
+		};
 		
-	// };
+ };
 }
 
 }
