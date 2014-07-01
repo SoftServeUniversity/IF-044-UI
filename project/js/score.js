3145cd22e715b2707b081a6a7b33ebab49061835
unknown <masterullo@gmail.com>
2014-07-01 06:04:10 +0300

    test result fixed

--- project/js/score.js
+++ project/js/score.js
@@ -18,12 +18,14 @@ document.getElementById('answerpanel').innerHTML = "";
 var place_for_answer = document.getElementById('answerpanel');
 //console.log(place_for_answer);
 var answerArray = JSON.parse(localStorage.QuestionObject);
+var n = answerArray.Test_id;
 
 	function getCorrectAnswerByQuestionId(test_id, correct_answer_id){
-		for(var i = 0; i<Model.date.Tests[0].answers.length; i++){
-				if(Model.date.Tests[0].answers[i].id == correct_answer_id){
-					var catchCorrectAnswer = Model.date.Tests[0].answers[i].text_answer;
-					console.log(Model.date.Tests[0].answers[i].id+"<->"+correct_answer_id);
+		
+		for(var i = 0; i<Model.date.Tests[n].answers.length; i++){
+				if(Model.date.Tests[n].answers[i].id == correct_answer_id){
+					var catchCorrectAnswer = Model.date.Tests[n].answers[i].text_answer;
+					console.log(Model.date.Tests[n].answers[i].id+"<->"+correct_answer_id);
 				}
 				else{
 				}
@@ -46,18 +48,18 @@ var answerArray = JSON.parse(localStorage.QuestionObject);
 		// }
 		console.log(answerArray.length);
 
-
-		for(var l = 0; l < Model.date.Tests[0].question.length; l++){
-			for(var i = 0; i<Model.date.Tests[0].answers.length; i++){
-						if(Model.date.Tests[0].answers[i].id == correct_answer_id){
-							var ChooseCorrectAnswer = Model.date.Tests[0].answers[i].text_answer;
+		
+		for(var l = 0; l < Model.date.Tests[n].question.length; l++){
+			for(var i = 0; i<Model.date.Tests[n].answers.length; i++){
+						if(Model.date.Tests[n].answers[i].id == correct_answer_id){
+							var ChooseCorrectAnswer = Model.date.Tests[n].answers[i].text_answer;
 								
 								var empty = [];
-								for (var k = 0; k < Model.date.Tests[0].question.length; k++){
+								for (var k = 0; k < Model.date.Tests[n].question.length; k++){
 								empty.push(answerArray['question'+k][0]);
 								}
 								for (var x = 0; x < empty.length;  x++) {
-										if (empty[x] == Model.date.Tests[0].answers[i].text_answer) {
+										if (empty[x] == Model.date.Tests[n].answers[i].text_answer) {
 											var color = 'blockcolor';
 											console.log(color);
 										}
@@ -94,10 +96,10 @@ var answerArray = JSON.parse(localStorage.QuestionObject);
 		var content_place = '';
 		Model.load_localStorage();
 		var number_question = 1;
-		 for(var i = 0; i < Model.date.Tests[0].question.length; i++){
-		 	for(var j = 0; j < Model.date.Tests[0].correct_answer.length; j++){
-		 		if(Model.date.Tests[0].correct_answer[j].question_id == Model.date.Tests[0].question[i].id){
-		 			var correct_answer_id = Model.date.Tests[0].correct_answer[j].answer_id;
+		 for(var i = 0; i < Model.date.Tests[n].question.length; i++){
+		 	for(var j = 0; j < Model.date.Tests[n].correct_answer.length; j++){
+		 		if(Model.date.Tests[n].correct_answer[j].question_id == Model.date.Tests[n].question[i].id){
+		 			var correct_answer_id = Model.date.Tests[n].correct_answer[j].answer_id;
 		 			//console.log(correct_answer_id);
 		 		}
 
@@ -110,15 +112,14 @@ var answerArray = JSON.parse(localStorage.QuestionObject);
 				// 	}
 					
 
-		 	content_place += "<div class='col-xs-12 "+ChooseCorrectAnswer(0, correct_answer_id)+"'><div class='col-xs-12 question'><strong>"+number_question+++('. ')+"<span class='questiontext'>"+Model.date.Tests[0].question[i].text+"</span></strong></div><div class='col-xs-12 answer'><strong>Відповідь: </strong><span class='result'>"+answerArray['question'+i]+"</span></div><div class='col-xs-12 right-answer'><strong>Правильна відповідь: </strong><span class='true'>"+getCorrectAnswerByQuestionId(0, correct_answer_id)+"</span></div><div class='more-about'><strong>Пояснення: </strong><span>"+Model.date.Tests[0].description+"</span></div></div>"
+		 	content_place += "<div class='col-xs-12 "+ChooseCorrectAnswer(0, correct_answer_id)+"'><div class='col-xs-12 question'><strong>"+number_question+++('. ')+"<span class='questiontext'>"+Model.date.Tests[n].question[i].text+"</span></strong></div><div class='col-xs-12 answer'><strong>Відповідь: </strong><span class='result'>"+answerArray['question'+i]+"</span></div><div class='col-xs-12 right-answer'><strong>Правильна відповідь: </strong><span class='true'>"+getCorrectAnswerByQuestionId(0, correct_answer_id)+"</span></div><div class='more-about'><strong>Пояснення: </strong><span>"+Model.date.Tests[n].question.question_description+"</span></div></div>"
 		 }
 		 place_for_answer.innerHTML = content_place;
 	}
 	showAll();
 
 	function pass_again(){
-		localStorage.clear();
-		window.location='Test.html?id=1'
+		window.location='Test.html?id='+n+''
 	}
 
 //  	var answerArray = {};
