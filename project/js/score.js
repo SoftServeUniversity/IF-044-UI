678573da6191bf5c4fcfe77a6cd618e94d7742af
ichyr <ichyr@softserveinc.com>
2014-07-01 08:06:50 +0300

    score.html fix with test id

--- project/js/score.js
+++ project/js/score.js
@@ -1,126 +1,125 @@
 window.onload = calculate;
-function calculate(){
- 	var questions = document.getElementsByClassName('question').length;
- 	var b = 100/questions;
- 	var h = Math.round(b);
- 	var rightAnswers = document.getElementsByClassName('blockcolor').length;
- 	var result = rightAnswers*h;
- 	var correctPercentage = (rightAnswers *100)/ questions;
- 	var rightEl = document.getElementById('right');
- 	var wrongEl = document.getElementById('wrong');
- 	var total = document.getElementById('total');
- 	total.innerHTML = result + ' з ' + 100;
- 	rightEl.style.width = correctPercentage + '%';
-	wrongEl.style.width = (100 - correctPercentage) + '%';
- }
+
+function calculate() {
+    var questions = document.getElementsByClassName('question').length;
+    var b = 100 / questions;
+    var h = Math.round(b);
+    var rightAnswers = document.getElementsByClassName('blockcolor').length;
+    var result = rightAnswers * h;
+    var correctPercentage = (rightAnswers * 100) / questions;
+    var rightEl = document.getElementById('right');
+    var wrongEl = document.getElementById('wrong');
+    var total = document.getElementById('total');
+    total.innerHTML = result + ' з ' + 100;
+    rightEl.style.width = correctPercentage + '%';
+    wrongEl.style.width = (100 - correctPercentage) + '%';
+}
 
 document.getElementById('answerpanel').innerHTML = "";
 var place_for_answer = document.getElementById('answerpanel');
 //console.log(place_for_answer);
 var answerArray = JSON.parse(localStorage.QuestionObject);
-var n = answerArray.Test_id;
 
-	function getCorrectAnswerByQuestionId(test_id, correct_answer_id){
-		
-		for(var i = 0; i<Model.date.Tests[n].answers.length; i++){
-				if(Model.date.Tests[n].answers[i].id == correct_answer_id){
-					var catchCorrectAnswer = Model.date.Tests[n].answers[i].text_answer;
-					console.log(Model.date.Tests[n].answers[i].id+"<->"+correct_answer_id);
-				}
-				else{
-				}
-				//console.log(catchCorrectAnswer);
-				// if (catchCorrectAnswer == answerArray['question'+i]) {
-				// 	var val = answerArray['question'+i];
-				// 	console.log(val);
+// test id starts with 1, but array elements start with 0
+var n = answerArray.Test_id - 1;
 
-				// };
-		}	
-		return catchCorrectAnswer;
-	}
+function getCorrectAnswerByQuestionId(test_id, correct_answer_id) {
 
-	function ChooseCorrectAnswer(test_id, correct_answer_id){
+    for (var i = 0; i < Model.date.Tests[n].answers.length; i++) {
+        if (Model.date.Tests[n].answers[i].id == correct_answer_id) {
+            var catchCorrectAnswer = Model.date.Tests[n].answers[i].text_answer;
+            console.log(Model.date.Tests[n].answers[i].id + "<->" + correct_answer_id);
+        } else {}
+        //console.log(catchCorrectAnswer);
+        // if (catchCorrectAnswer == answerArray['question'+i]) {
+        // 	var val = answerArray['question'+i];
+        // 	console.log(val);
+
+        // };
+    }
+    return catchCorrectAnswer;
+}
+
+function ChooseCorrectAnswer(test_id, correct_answer_id) {
 
 
-		// for (var l = 0; l < answerArray.length; l++){
-			
+    // for (var l = 0; l < answerArray.length; l++){
 
-		// }
-		console.log(answerArray.length);
 
-		
-		for(var l = 0; l < Model.date.Tests[n].question.length; l++){
-			for(var i = 0; i<Model.date.Tests[n].answers.length; i++){
-						if(Model.date.Tests[n].answers[i].id == correct_answer_id){
-							var ChooseCorrectAnswer = Model.date.Tests[n].answers[i].text_answer;
-								
-								var empty = [];
-								for (var k = 0; k < Model.date.Tests[n].question.length; k++){
-								empty.push(answerArray['question'+k][0]);
-								}
-								for (var x = 0; x < empty.length;  x++) {
-										if (empty[x] == Model.date.Tests[n].answers[i].text_answer) {
-											var color = 'blockcolor';
-											console.log(color);
-										}
-										else{
-											var color = 'blockcolorwrong';
-											console.log(color);
-										}
-								}							
+    // }
+    console.log(answerArray.length);
 
-								// if ( == answerArray['question'+l]) {
-								// 	var color = 'blockcolor';
-								// 	console.log(color);
-								}
-								// else{
-								// 	var color = 'blockcolorwrong';
-								// 	console.log(color);
-								// }
-								//console.log(Model.date.Tests[0].answers[i].id+"<->"+correct_answer_id);
-							
-						else{
-						
-						}
-						//console.log(catchCorrectAnswer);
-						
-				}	
-				
-			}
-			return color;
-		}
 
-	function showAll(){
-		//console.log(Model.date.Tests[0].question.length);
+    for (var l = 0; l < Model.date.Tests[n].question.length; l++) {
+        for (var i = 0; i < Model.date.Tests[n].answers.length; i++) {
+            if (Model.date.Tests[n].answers[i].id == correct_answer_id) {
+                var ChooseCorrectAnswer = Model.date.Tests[n].answers[i].text_answer;
 
-		var content_place = '';
-		Model.load_localStorage();
-		var number_question = 1;
-		 for(var i = 0; i < Model.date.Tests[n].question.length; i++){
-		 	for(var j = 0; j < Model.date.Tests[n].correct_answer.length; j++){
-		 		if(Model.date.Tests[n].correct_answer[j].question_id == Model.date.Tests[n].question[i].id){
-		 			var correct_answer_id = Model.date.Tests[n].correct_answer[j].answer_id;
-		 			//console.log(correct_answer_id);
-		 		}
+                var empty = [];
+                for (var k = 0; k < Model.date.Tests[n].question.length; k++) {
+                    empty.push(answerArray['question' + k][0]);
+                }
+                for (var x = 0; x < empty.length; x++) {
+                    if (empty[x] == Model.date.Tests[n].answers[i].text_answer) {
+                        var color = 'blockcolor';
+                        console.log(color);
+                    } else {
+                        var color = 'blockcolorwrong';
+                        console.log(color);
+                    }
+                }
 
-		 	}
-		 	// getCorrectAnswerByQuestionId(0, correct_answer_id);
-				// if (catchCorrectAnswer == answerArray['question'+i]) {
-				// 	var color = blockcolor;}
-				// 	else{
-				// 		var color = blockcolor_wrong;
-				// 	}
-					
+                // if ( == answerArray['question'+l]) {
+                // 	var color = 'blockcolor';
+                // 	console.log(color);
+            }
+            // else{
+            // 	var color = 'blockcolorwrong';
+            // 	console.log(color);
+            // }
+            //console.log(Model.date.Tests[0].answers[i].id+"<->"+correct_answer_id);
+            else {
 
-		 	content_place += "<div class='col-xs-12 "+ChooseCorrectAnswer(0, correct_answer_id)+"'><div class='col-xs-12 question'><strong>"+number_question+++('. ')+"<span class='questiontext'>"+Model.date.Tests[n].question[i].text+"</span></strong></div><div class='col-xs-12 answer'><strong>Відповідь: </strong><span class='result'>"+answerArray['question'+i]+"</span></div><div class='col-xs-12 right-answer'><strong>Правильна відповідь: </strong><span class='true'>"+getCorrectAnswerByQuestionId(0, correct_answer_id)+"</span></div><div class='more-about'><strong>Пояснення: </strong><span>"+Model.date.Tests[n].question.question_description+"</span></div></div>"
-		 }
-		 place_for_answer.innerHTML = content_place;
-	}
-	showAll();
+            }
+            //console.log(catchCorrectAnswer);
 
-	function pass_again(){
-		window.location='Test.html?id='+n+''
-	}
+        }
+
+    }
+    return color;
+}
+
+function showAll() {
+    //console.log(Model.date.Tests[0].question.length);
+
+    var content_place = '';
+    Model.load_localStorage();
+    var number_question = 1;
+    for (var i = 0; i < Model.date.Tests[n].question.length; i++) {
+        for (var j = 0; j < Model.date.Tests[n].correct_answer.length; j++) {
+            if (Model.date.Tests[n].correct_answer[j].question_id == Model.date.Tests[n].question[i].id) {
+                var correct_answer_id = Model.date.Tests[n].correct_answer[j].answer_id;
+                //console.log(correct_answer_id);
+            }
+
+        }
+        // getCorrectAnswerByQuestionId(0, correct_answer_id);
+        // if (catchCorrectAnswer == answerArray['question'+i]) {
+        // 	var color = blockcolor;}
+        // 	else{
+        // 		var color = blockcolor_wrong;
+        // 	}
+
+
+        content_place += "<div class='col-xs-12 " + ChooseCorrectAnswer(0, correct_answer_id) + "'><div class='col-xs-12 question'><strong>" + number_question+++('. ') + "<span class='questiontext'>" + Model.date.Tests[n].question[i].text + "</span></strong></div><div class='col-xs-12 answer'><strong>Відповідь: </strong><span class='result'>" + answerArray['question' + i] + "</span></div><div class='col-xs-12 right-answer'><strong>Правильна відповідь: </strong><span class='true'>" + getCorrectAnswerByQuestionId(0, correct_answer_id) + "</span></div><div class='more-about'><strong>Пояснення: </strong><span>" + Model.date.Tests[n].question.question_description + "</span></div></div>"
+    }
+    place_for_answer.innerHTML = content_place;
+}
+showAll();
+
+function pass_again() {
+    window.location = 'Test.html?id=' + n + ''
+}
 
 //  	var answerArray = {};
 //  	answerArray = JSON.parse(localStorage.QuestionObject);
@@ -132,7 +131,7 @@ var n = answerArray.Test_id;
 
 //  	var TestStructure = function(testNum){
 //  	var testNum = 1;
- 	
+
 //  		var num = document.getElementsByClassName('questiontext');
 //  		var rightanswer = document.getElementsByClassName('true')
 //  			for(var i = 0; i < Tests[testNum].question.length; i++){
