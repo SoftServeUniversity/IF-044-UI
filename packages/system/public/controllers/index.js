f6a0c2ebc4adf74b9d99ad84520d23830d676440
Serhiy Yosipenko <serhiy.yosipenko@gmail.com>
2014-08-08 22:07:42 +0300

    all in one

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -7,7 +7,7 @@
 
 angular.module('mean.system').controller('IndexController', ['$scope','$http', function ($scope, $http) {
   
-	    $http.get('http://localhost:3000/indexdata').success(function(result) {
+	    $http.get('/indexdata').success(function(result) {
 	        $scope.tests = result;
 	    });
 }]);
--- packages/test/public/controllers/test.js
+++ packages/test/public/controllers/test.js
@@ -2,7 +2,7 @@
 
 angular.module('mean.test').controller('TestController', ['$scope', '$http', '$stateParams',
     function($scope, $http, $stateParams) {
-        $http.get('http://localhost:3000/test/' + $stateParams.id).success(function(result) {
+        $http.get('/test/' + $stateParams.id).success(function(result) {
             $scope.tests = result[0];
             $scope.name = result[1];
         });
